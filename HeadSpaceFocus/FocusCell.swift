ebe94b88de3b06394fe259235f2f331535f675ba
tony-yun <lifeyun24@naver.com>
2023-10-11 23:23:34 +0900

    Initial Commit

---
+++ HeadSpaceFocus/FocusCell.swift
@@ -0,0 +1,21 @@
+//
+//  FocusCell.swift
+//  HeadSpaceFocus
+//
+//  Created by 윤태웅 on 10/11/23.
+//
+
+import UIKit
+
+class FocusCell: UICollectionViewCell {
+    
+    @IBOutlet weak var weatherImage: UIImageView!
+    @IBOutlet weak var descriptionLabel: UILabel!
+    @IBOutlet weak var titleLabel: UILabel!
+    
+    func configure(_ focus: Focus) {
+        weatherImage.image = UIImage(named: focus.imageName)
+        descriptionLabel.text = focus.description
+        titleLabel.text = focus.title
+    }
+}
---
+++ HeadSpaceFocus/FocusViewController.swift
@@ -0,0 +1,16 @@
+//
+//  FocusViewController.swift
+//  HeadSpaceFocus
+//
+//  Created by 윤태웅 on 10/11/23.
+//
+
+import UIKit
+
+class FocusViewController: UIViewController {
+
+    @IBOutlet weak var collectionView: UICollectionView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
